27ded07dc2d5125403fc0b81aaa449704507b7b8
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-06-29 14:02:45 +0300

    Adding download & upload functionality

--- node-3/dist/controllers/fileControllers.js
+++ node-3/dist/controllers/fileControllers.js
@@ -17,8 +17,6 @@ const fs_extra_1 = __importDefault(require("fs-extra"));
 const uploadFile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     req.pipe(req.busboy);
     const { fileId, index } = req.query;
-    console.log(fileId);
-    console.log(index);
     req.busboy.on("file", (fieldName, file, filename) => {
         console.log(`Upload of '${fileId}' started`);
         // Create a write stream of the new file
@@ -36,23 +34,16 @@ const uploadFile = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     });
 });
 exports.uploadFile = uploadFile;
+//Downloading file from node
 const downloadFile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { fileId, index } = req.query;
     try {
         const buffer = yield fs_extra_1.default.readFile(`${__dirname}/../../files/${fileId}=${index}`);
         const base64 = buffer.toString("base64");
-        console.log("buffer 3 finished");
         res.status(200).json(base64);
     }
     catch (error) {
         res.status(500).json({ error: error.message });
     }
-    // res
-    //   .status(200)
-    //   .sendFile(`${__dirname}/../../files/${fileId}=${index}`, {}, (err) => {
-    //     if (err) {
-    //       return res.status(400).json({ error: err });
-    //     }
-    //   });
 });
 exports.downloadFile = downloadFile;
